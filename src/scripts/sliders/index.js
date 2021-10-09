041176a704948fd1e19e2b223b39c58069906826
spoonya <nomad8t@gmail.com>
2021-10-09 15:23:14 +0300

    feat: implement prevent

---
+++ src/scripts/sliders/index.js
@@ -0,0 +1,1 @@
+export { default as swiperPrevent } from './prevent';
---
+++ src/scripts/sliders/prevent.js
@@ -0,0 +1,42 @@
+const swiperPrevent = new Swiper('#swiper-prevent', {
+  slidesPerColumnFill: 'row',
+
+  loop: true,
+
+  spaceBetween: 20,
+
+  autoplay: {
+    delay: 4000
+  },
+
+  grabCursor: true,
+
+  resizeObserver: true,
+  observer: true,
+  observeParents: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerGroup: 1,
+      slidesPerColumn: 1,
+      slidesPerView: 1
+    },
+    767: {
+      slidesPerGroup: 1,
+      slidesPerColumn: 1,
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerGroup: 1,
+      slidesPerColumn: 1,
+      slidesPerView: 3
+    }
+  }
+});
+
+export default swiperPrevent;
